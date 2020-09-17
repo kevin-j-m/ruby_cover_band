248636b4ba5d028c5e4b59e63e7a6351e9172148
Kevin Murphy <murphy.kevin.mail@gmail.com>
2020-09-16 22:51:06 -0400

    way of collecting broken strings that isn't depending on particular file path structure

--- lib/line_coverage_results.rb
+++ lib/line_coverage_results.rb
@@ -9,12 +9,6 @@ result = Coverage.result
 # p result
 
 string_results = result.select { |k, v| k.end_with?("guitar/string.rb") }
-# puts string_results
-# puts string_results.first
-
-original_string_lines = result.dig("/Users/kevin/dev/kevin-j-m/ruby_cover_band/lib/ruby_cover_band/instruments/guitar/string.rb", :lines)
-string_lines = string_results.values.first[:lines]#result.dig("/Users/kevin/dev/kevin-j-m/ruby_cover_band/lib/ruby_cover_band/instruments/guitar/string.rb", :lines)
-# puts string_lines
+string_lines = string_results.values.first[:lines]
 
 puts "Number of broken strings: #{string_lines[51]}"
-puts "Number of broken strings: #{original_string_lines[51]}"
